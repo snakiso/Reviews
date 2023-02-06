079ce5cc9be624b854afda3e9e8514376aafeb5f
igorchagov <igorchagov@mail.ru>
2023-02-06 15:51:15 +0300

    add

---
+++ js/app.js
@@ -0,0 +1,102 @@
+let reviews = document.querySelector('.reviews__block');
+let moreReviews = document.querySelector('.reviews__more');
+let i = 0
+let count = 6;
+
+moreReviews.addEventListener('click', () => {
+ count += 3;
+ createElem()
+})
+
+async function createElem() {
+ // 
+ let url = `./files/reviews.json`
+ let response = await fetch(url)
+ let json = await response.json();
+ //
+ // итерация по json файлу
+ if (count > json.length) {
+  moreReviews.style.display = "none"
+ }
+ for (i; i < count; i++) {
+
+  // создание контейнера   
+  let reviewBox = document.createElement("div");
+  reviewBox.className = 'reviews__box';
+
+  //Контейнер для фото и рейтинга
+  let reviewerHead = document.createElement("div");
+  reviewerHead.className = 'reviewer-head';
+
+  //Контейнер для информации о человеке (Имя и Возраст)
+  let reviewerInfo = document.createElement("div");
+  reviewerInfo.className = 'reviewer-info';
+
+  //Создание оценки 
+  let grade = document.createElement("div");
+  grade.className = 'reviewer-grade';
+
+
+  //Заполнение оценки
+  let gradeFill = document.createElement("span");
+  gradeFill.className = 'reviewer-grade-fill';
+  gradeFill.style.width = `${20 * json[i].Rating}%`
+  //Создание имени
+  let name = document.createElement("span");
+  name.className = 'reviewer-name';
+  name.textContent = json[i].Name;
+
+  //Cоздание возраста
+  let age = document.createElement("span");
+  age.className = 'reviewer-age';
+  age.textContent = json[i].Age;
+
+  //Создание услуги 
+  let service = document.createElement("span");
+  service.className = 'reviewer-service';
+  service.textContent = json[i].Service;
+
+  //Какая услуга 
+
+  let serviceDetails = document.createElement("span");
+  serviceDetails.className = 'reviewer-service-details';
+  serviceDetails.textContent = `${json[i].ServiceDetails} ${json[i].Graft}`;
+
+  //Добавление фото 
+  let photo = document.createElement('img');
+  photo.className = 'reviewer-photo';
+  photo.src = json[i].Photo;
+
+
+  //Создание текста   
+  let p = document.createElement('p');
+  p.className = 'reviewer-text'
+  p.textContent = json[i].Review
+
+  //Создание кнопки  'Подробнее'  
+  let button = document.createElement('a');
+  button.href = json[i].Page
+  button.className = 'review-details'
+  button.innerHTML = 'Подробнее'
+
+
+
+
+
+  //Добавление в верстку
+  grade.appendChild(gradeFill)
+  reviewerHead.appendChild(photo)
+  reviewerHead.appendChild(grade)
+  reviewerInfo.appendChild(name)
+  reviewerInfo.appendChild(age)
+  reviewBox.appendChild(reviewerHead)
+  reviewBox.appendChild(reviewerInfo)
+  reviewBox.appendChild(service)
+  reviewBox.appendChild(serviceDetails)
+  reviewBox.appendChild(p)
+  reviewBox.appendChild(button)
+  reviews.appendChild(reviewBox)
+ }
+
+}
+createElem()
